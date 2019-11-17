1c8fa349902f07722bc1df3f21d3002ec590cd37
Maimell <malinka77@gmail.com>
2019-11-16 17:39:15 -0700

    the whole structure is ready

---
+++ models/burger.js
@@ -0,0 +1,4 @@
+//Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+const orm = require('../config/orm');
+
+//Export at the end of the burger.js file.
--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
-
-
-
 const handlebars = require('express-handlebars');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
+app.listen(PORT, function () {
+    console.log(`We are on http://localhost:${PORT}`);
+})
